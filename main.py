683416a6d1fb3eefba2dc7a5a484ce303d7c5cd4
hhokari <57063298+hhokari@users.noreply.github.com>
2020-04-12 21:26:43 -0700

    I made these changes.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from main_funcs import *
+import logging
+from logging.config import fileConfig
+
+fileConfig("logging_config.ini")
+
+logger = logging.getLogger("main")
+
+logger.info("Application started")
+
 word = input("Name of event: ")
 recipient = input("To whom is this photo for? ")
 res = main_func(word, recipient)
-print(res)
